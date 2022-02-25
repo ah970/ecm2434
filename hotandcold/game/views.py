cbb9a42f04a6d5f3917969b7a32352bf108dad98
ah970 <91027708+ah970@users.noreply.github.com>
2022-02-25 14:18:15 +0000

    Added dummy views

--- hotandcold/game/views.py
+++ hotandcold/game/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Home page.")
+
+
+def login(request):
+    return HttpResponse("Login page.")
+
+
+def register(request):
+    return HttpResponse("Registration page.")
+
+
+def game(request):
+    return HttpResponse("Game page.")
