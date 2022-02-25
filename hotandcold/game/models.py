f9b611602f63b14f9f8da63cf1113ce330ce6453
CharliePW <charlie.wright1212@gmail.com>
2022-02-25 22:28:15 +0000

    added the User model

--- hotandcold/game/models.py
+++ hotandcold/game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create the user model
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
