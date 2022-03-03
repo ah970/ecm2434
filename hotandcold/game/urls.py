dd079d2bf5c0ea5daa922a6f82a19e4f37dac2a4
ah970 <91027708+ah970@users.noreply.github.com>
2022-03-03 00:42:26 +0000

    Properly namespaced URLs

--- hotandcold/game/urls.py
+++ hotandcold/game/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("test/", views.test, name="test"),
-    path("login/", views.log_in, name="log_in"),
-    path("logout/", views.log_out, name="log_out"),
+    path("login/", views.log_in, name="login"),
+    path("logout/", views.log_out, name="logout"),
     path("register/", views.register, name="register"),
     path("game/", views.game, name="game"),
     path("create_event/", views.create_event, name="create event"),
